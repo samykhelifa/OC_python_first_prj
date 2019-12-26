64ff8cadd487d4c42d213d91c8135204c620eaba
samy <39716963+samykhelifa@users.noreply.github.com>
2019-12-26 18:36:01 +0100

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,1 +1,25 @@
+# -*- coding: utf8 -*-
+
+quotes = [
+    "Ecoutez-moi, Monsieur Shakespeare, nous avons beau être ou ne pas être, nous sommes !",
+    "On doit pouvoir choisir entre s'écouter parler et se faire entendre."
+]
+
+characters = [
+    "alvin et les Chipmunks",
+    "Babar",
+    "betty boop",
+    "calimero",
+    "casper",
+    "le chat potté",
+    "Kirikou"
+]
+
+if user_answer == "B":
+    pass
+elif user_answer == "C":
+    print("C pas la bonne réponse ! Et G pas d’humour, je C...")
+else:
+    # show another quote
+    pass
 
