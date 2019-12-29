3b3bab519bd17bdd364b378775dc5fd37b7ea8f0
samy <39716963+samykhelifa@users.noreply.github.com>
2019-12-29 19:10:08 +0100

    Update Test.py

--- Test.py
+++ Test.py
@@ -2,4 +2,5 @@ quotes=["jkh","a","yu","ze"]
 for q in quotes:
     print(q.capitalize())
     
-"{} a dit : {}".format("Babar", "Tout n'est pas cirrhose dans la vie, comme dit l'alcoolique.")
+form = "{} a dit : {}".format("Babar", "Tout n'est pas cirrhose dans la vie, comme dit l'alcoolique.")
+print(form)
